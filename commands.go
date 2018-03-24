6009f3b5bc8e4ab27e7462d02d6b10b84dc1ca94
Szilárd Mátis <matis.szilard@codespring.ro>
2018-03-24 12:20:33 +0200

    Added safecheck to eLikeABoss functionality

--- commands.go
+++ commands.go
@@ -5,18 +5,30 @@ import (
 	"os/exec"
 )
 
+var isRunning = false
+
 func startImperialMarch() {
+	if isRunning {
+		return
+	}
 	cmd := exec.Command("spotify", "play", "uri", "spotify:track:6ht3MI8a40UT5haN7t9StM")
 	err := cmd.Run()
 	if err != nil {
 		log.Printf("Command finished with error: %v", err)
+		return
 	}
+	isRunning = true
 }
 
 func stopImperialMarch() {
+	if !isRunning {
+		return
+	}
 	cmd := exec.Command("spotify", "stop")
 	err := cmd.Run()
 	if err != nil {
 		log.Printf("Command finished with error: %v", err)
+		return
 	}
+	isRunning = false
 }
